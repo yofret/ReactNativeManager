d9c3fda37fb872a3cab2e9eee1a18a4854d2bd8a
Yofret Rios <yofret.rios@jobsity.com>
2017-03-15 10:09:36 -0500

    Adding firebase

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@
 	import { View, Text } from 'react-native';
 	import { Provider } from 'react-redux';
 	import { createStore } from 'redux';
+	import firebase from 'firebase';
+	import reducers from './reducers';
 
 
 //-------------------------------
@@ -13,9 +15,22 @@
 //-------------------------------
 	
 	class App extends Component {
+
+		componentWillMount() {
+			const config = {
+				apiKey: 'AIzaSyChPko5hStWUj-WolROcG50jsjfaHu5FKY',
+				authDomain: 'manager-cc5da.firebaseapp.com',
+				databaseURL: 'https://manager-cc5da.firebaseio.com',
+				storageBucket: 'manager-cc5da.appspot.com',
+				messagingSenderId: '739200810786'
+			};
+
+			firebase.initializeApp(config);
+		}			
+
 		render() {
 			return (
-				<Provider store={createStore()}>
+				<Provider store={createStore(reducers)}>
 					<View>
 						<Text>Hello!!</Text>
 					</View>
