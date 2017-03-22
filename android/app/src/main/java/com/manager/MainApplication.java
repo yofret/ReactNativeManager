7f56d08b7c790b659428a45c0f93e23195f9a35a
Yofret Rios <yofret.rios@jobsity.com>
2017-03-22 09:41:07 -0500

    Trying to implemente Native Navigator

--- android/app/src/main/java/com/manager/MainApplication.java
+++ android/app/src/main/java/com/manager/MainApplication.java
@@ -1,40 +1,23 @@
 package com.manager;
 
-import android.app.Application;
+import android.support.annotation.Nullable;
 
-import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.shell.MainReactPackage;
-import com.facebook.soloader.SoLoader;
+import com.reactnativenavigation.NavigationApplication;
 
-import java.util.Arrays;
+
 import java.util.List;
 
-public class MainApplication extends Application implements ReactApplication {
-
-  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
-    @Override
-    public boolean getUseDeveloperSupport() {
-      return BuildConfig.DEBUG;
-    }
+public class MainApplication extends NavigationApplication {
 
     @Override
-    protected List<ReactPackage> getPackages() {
-      return Arrays.<ReactPackage>asList(
-          new MainReactPackage()
-      );
+    public boolean isDebug() {
+        return BuildConfig.DEBUG;
     }
-  };
 
-  @Override
-  public ReactNativeHost getReactNativeHost() {
-    return mReactNativeHost;
-  }
-
-  @Override
-  public void onCreate() {
-    super.onCreate();
-    SoLoader.init(this, /* native exopackage */ false);
-  }
+    @Nullable
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return null;
+    }
 }
